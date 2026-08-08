4e02593bfc6aeb657d45ae19546d8ce058e4ef1a
yuniii-415 <seng_yun_hee@students.edu.sg>
2026-08-08 11:06:46 +0800

    feat: update torso to red rounded rectangle with SG flag

--- MascotMadness/Body/BodyView.swift
+++ MascotMadness/Body/BodyView.swift
@@ -3,9 +3,13 @@ import SwiftUI
 var torso: some BodyPart {
     Torso {
         ZStack {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundStyle(.green)
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .frame(width: 300, height: 350)
+                .foregroundStyle(.red)
+            
+            Text("🇸🇬")
+                .font(.system(size: 200))
         }
     }
 }
